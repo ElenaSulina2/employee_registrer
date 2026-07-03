e34a1cd1ea003a219ff7b108d66254682107d726
ElenaSulina2 <elenasulina93@mail.ru>
2026-07-03 13:03:39 +0300

    Добавить базовую модель сотрудников

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Date, CHAR
+from app.database import Base
+
+class Employee(Base):
+    __tablename__ = "employees"
+
+    id = Column(Integer, primary_key=True, index=True)
+    last_name = Column(String(50), nullable=False)
+    first_name = Column(String(50), nullable=False)
+    middle_name = Column(String(50), nullable=True)
+    birth_date = Column(Date, nullable=False)
+    gender = Column(CHAR(1), nullable=False)
+    phone = Column(String(20), nullable=True)
+    photo = Column(String(255), nullable=True)
